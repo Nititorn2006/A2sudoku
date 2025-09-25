a4e22997f4af290a0401171e6a6ea7f9d1bfed51
Nititorn2006 <nititornwongsuwan2006@gmail.com>
2025-09-25 23:33:25 +0700

    sudoku solver

--- sketch.js
+++ sketch.js
@@ -45,6 +45,96 @@ function drawNumbersButton(x, y, buttonWidth, buttonHeight, spacing) {
   }
 }
 
+const UNASSIGNED = 0;
+const N = 9;
+
+function findUnassignedLocation(grid, pos) {
+  let row = 0;
+  while (row < N) {
+    let col = 0;
+    while (col < N) {
+      if (grid[row][col] === UNASSIGNED) {
+        pos.row = row;
+        pos.col = col;
+        return true;
+      }
+      col++;
+    }
+    row++;
+  }
+  return false;
+}
+
+function usedInRow(grid, row, num) {
+  let col = 0;
+  while (col < N) {
+    if (grid[row][col] === num) return true;
+    col++;
+  }
+  return false;
+}
+
+function usedInCol(grid, col, num) {
+  let row = 0;
+  while (row < N) {
+    if (grid[row][col] === num) return true;
+    row++;
+  }
+  return false;
+}
+
+function usedInBox(grid, boxStartRow, boxStartCol, num) {
+  let row = 0;
+  while (row < 3) {
+    let col = 0;
+    while (col < 3) {
+      if (grid[row + boxStartRow][col + boxStartCol] === num) return true;
+      col++;
+    }
+    row++;
+  }
+  return false;
+}
+
+function isSafe(grid, row, col, num) {
+  return (
+    !usedInRow(grid, row, num) &&
+    !usedInCol(grid, col, num) &&
+    !usedInBox(grid, row - (row % 3), col - (col % 3), num) &&
+    grid[row][col] === UNASSIGNED
+  );
+}
+
+function solveSudoku(grid) {
+  let pos = { row: 0, col: 0 };
+  if (!findUnassignedLocation(grid, pos)) return true;
+  let row = pos.row;
+  let col = pos.col;
+  let num = 1;
+  while (num <= 9) {
+    if (isSafe(grid, row, col, num)) {
+      grid[row][col] = num;
+      if (solveSudoku(grid)) return true;
+      grid[row][col] = UNASSIGNED;
+    }
+    num++;
+  }
+  return false;
+}
+
+let grid = [
+  [5, 3, 0, 0, 7, 0, 0, 0, 0],
+  [6, 0, 0, 1, 9, 5, 0, 0, 0],
+  [0, 9, 8, 0, 0, 0, 0, 6, 0],
+  [8, 0, 0, 0, 6, 0, 0, 0, 3],
+  [4, 0, 0, 8, 0, 3, 0, 0, 1],
+  [7, 0, 0, 0, 2, 0, 0, 0, 6],
+  [0, 6, 0, 0, 0, 0, 2, 8, 0],
+  [0, 0, 0, 4, 1, 9, 0, 0, 5],
+  [0, 0, 0, 0, 8, 0, 0, 7, 9],
+];
+
+
 function draw() {
   background(220);
 
